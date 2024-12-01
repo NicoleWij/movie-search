22792c5ef7260252fa3446cd5774e2438b2372a2
NicoleWij <nicole.wijkman@gmail.com>
2024-12-01 03:54:37 +0100

    Fix JSX errors and clean node_module

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Movie Search!</h1>
+      <p>Start building your app here!</p>
+    </div>
+  );
+};
+
+export default App;
